f022a2614933e8bf727ae3303f623cf3a52bfbd5
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2013-10-13 22:01:23 +0200

    Changes to db models after input from Adi.

--- pmg_backend/models.py
+++ pmg_backend/models.py
@@ -28,64 +28,37 @@ class Bill(db.Model):
         return '<Bill: %r>' % str(self)
 
 
-association_table = db.Table('association', db.Base.metadata,
-  db.Column('agent_id', db.Integer, db.ForeignKey('agent.agent_id')),
-  db.Column('representative_id', db.Integer, db.ForeignKey('representative.representative_id'))
-)
+class Stage(db.Model):
+
+    stage_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500))
+
+    def to_dict(self):
+        # convert table row to dictionary
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __str__(self):
+        return str(self.stage_id) + " - " + self.name
+
+    def __repr__(self):
+        return '<Stage: %r>' % str(self)
 
 
 class Agent(db.Model):
 
     agent_id = db.Column(db.Integer, primary_key=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('role.role_id'))
-    role = db.relationship('Role', backref=db.backref('Agents', lazy='dynamic'))
-    representatives = db.relationship("Representative", secondary=association_table, backref="agents")
+    name = db.Column(db.String(500))
+    url = db.Column(db.String(500))
 
     def to_dict(self):
         # convert table row to dictionary
-        agent_dict = {c.name: getattr(self, c.name) for c in self.__table__.columns}
-        return agent_dict
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __str__(self):
-        return str(self.agent_id) + " - " + str(self.role.name)
+        return str(self.agent_id) + " - " + self.name
 
     def __repr__(self):
         return '<Agent: %r>' % str(self)
-
-
-class Representative(db.Model):
-
-    representative_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(500))
-    title = db.Column(db.String(100))
-    gender = db.Column(db.String(1))
-    political_party = db.Column(db.String(100))
-
-    def to_dict(self):
-        # convert table row to dictionary
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-    def __str__(self):
-        return str(self.representative_id) + " - " + " " + self.name
-
-    def __repr__(self):
-        return '<Representative: %r>' % str(self)
-
-
-class Role(db.Model):
-
-    role_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(500), unique=True)
-
-    def to_dict(self):
-        # convert table row to dictionary
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-    def __str__(self):
-        return str(self.role_id) + " - " + " " + self.name
-
-    def __repr__(self):
-        return '<Role: %r>' % str(self)
 
 
 class Location(db.Model):
@@ -147,10 +120,11 @@ class Event(db.Model):
 class Resolution(db.Model):
 
     resolution_id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(100), unique=True)
-    outcome = db.Column(db.Boolean)
+    type = db.Column(db.String(100))
+    outcome = db.Column(db.String(100))
     count_for = db.Column(db.Integer)
     count_against = db.Column(db.Integer)
+    count_abstain = db.Column(db.Integer)
 
     def to_dict(self):
         # convert table row to dictionary
@@ -184,36 +158,18 @@ class Session(db.Model):
 class Revision(db.Model):
 
     revision_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500))
+    url = db.Column(db.String(500))
 
     def to_dict(self):
         # convert table row to dictionary
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __str__(self):
-        return str(self.revision_id)
+        return str(self.revision_id) + " - " + self.name
 
     def __repr__(self):
         return '<Revision: %r>' % str(self)
-
-
-class Version(db.Model):
-
-    version_id = db.Column(db.Integer, primary_key=True)
-    revision_id = db.Column(db.Integer, db.ForeignKey('revision.revision_id'))
-    revision = db.relationship('Revision', backref=db.backref('versions', lazy='dynamic'))
-    name = db.Column(db.String(500))
-    url = db.Column(db.String(500))
-
-    def to_dict(self):
-        # convert table row to dictionary
-        version_dict = {c.name: getattr(self, c.name) for c in self.__table__.columns}
-        return version_dict
-
-    def __str__(self):
-        return str(self.version_id) + " - " + " " + self.name
-
-    def __repr__(self):
-        return '<Version: %r>' % str(self)
 
 
 class Content(db.Model):
--- rebuild_db.py
+++ rebuild_db.py
@@ -1,3 +1,4 @@
+import datetime
 def rebuild_db():
     """
     Drop and then rebuild the current database, populating it with some test data.
@@ -7,52 +8,121 @@ def rebuild_db():
     db.drop_all()
     db.create_all()
 
-    from pmg_backend.models import *
+    from pmg_backend.models import Bill, Agent, Location, Stage, Event, Content, Resolution, Revision, Session
 
     b1 = Bill()
-    b1.name = 'Test Bill'
-    b1.bill_type = 'Section 75'
-    b1.objective = 'The aim of this bill is to demonstrate the functionality of the PMG Bill Tracker application.'
+    b1.name = 'Protection of State Information Bill'
+    b1.bill_type = 'Section 75 (Ordinary Bills not affecting provinces)'
+    b1.objective = 'To provide for the protection of certain information from destruction, loss or unlawful disclosure; to regulate the manner in which information may be protected; to repeal the Protection of Information Act, 1982; and to provide for matters connected therewith.'
     db.session.add(b1)
 
-    a1 = Agent()
-    a1.name = 'National Assembly'
-    a2 = Agent()
-    a2.name = 'National Council of Provinces'
-    a3 = Agent()
-    a3.name = 'Ad-Hoc committee on the Develpment of Applications.'
-    a4 = Agent()
-    a4.name = 'Minister of Application Development'
-    db.session.add(a1)
-    db.session.add(a2)
-    db.session.add(a3)
-    db.session.add(a4)
+    locations = [
+        "National Assembly (NA)",
+        "National Council of Provinces (NCOP)",
+        "The Office of the President",
+    ]
 
-    v1 = Version()
-    v1.bill = b1
-    v1.code = 'B1 2012'
-    v1.url = 'http://example.com'
-    v2 = Version()
-    v2.bill = b1
-    v2.code = 'B2 2012'
-    v2.url = 'http://example.com'
-    db.session.add(v1)
-    db.session.add(v2)
+    for tmp in locations:
+        location = Location()
+        location.name = tmp
+        db.session.add(location)
 
-    e1 = Event()
-    e1.bill = b1
-    e1.agent = a4
-    e1.event_type = "A"
-    e1.new_status = "Introduced to Parliament"
-    e1.new_version = v1
-    db.session.add(e1)
+    agents = [
+        "National Assembly (NA)",
+        "NA Committee",
+        "National Council of Provinces (NCOP)",
+        "NCOP Committee",
+        "Joint Committee",
+        "Minister",
+        "President",
+        "Member of Parliament",
+    ]
 
-    c1 = SupportingContent()
-    c1.event = e1
-    c1.title = "Explanatory memorandum"
-    c1.description = "This is a brief description of the document."
-    c1.url = "http://example.com"
-    db.session.add(c1)
+    for tmp in agents:
+        agent = Agent()
+        agent.name = tmp
+        db.session.add(agent)
+
+    stages = [
+        "Waiting to be introduced to National Assembly",
+        "Introduced to National Assembly",
+        "Assigned to National Assembly Committee",
+        "Being reviewed by National Assembly Committee",
+        "Awaiting vote in National Assembly",
+        "Waiting to be introduced to National Council of Provinces"
+        "Introduced to National Council of Provinces",
+        "Assigned to NCOP Committee",
+        "Being reviewed by NCOP Committee",
+        "Awaiting vote in National Council of Provinces",
+        "Assigned to Mediation committee",
+        "Dropped",
+        "Waiting to be signed into law",
+        "Signed into law",
+    ]
+
+    for tmp in stages:
+        stage = Stage()
+        stage.name = tmp
+        db.session.add(stage)
+
+    content = [
+        ("memorandum", "Explanatory Memorandum", "uploads/memo-1.html"),
+        ("greenpaper", "Green Paper", "uploads/greenpaper.pdf"),
+        ("whitepaper", "White Paper", "uploads/whitepaper.pdf"),
+        ("draft", "Draft Bill", "uploads/draft.pdf"),
+        ("pmg-meeting-report", "Meeting report: 3 May 2013", "uploads/pmg-report-1.pdf"),
+        ("pmg-meeting-report", "Meeting report: 6 May 2012 - Public Hearings", "uploads/pmg-report-2.pdf"),
+        ("pmg-meeting-report", "Meeting report: 20 June 2011", "uploads/pmg-report-1.pdf"),
+        ("committee-report", "Committee Report", "uploads/committee-report-1.pdf"),
+        ("hansard-minutes", "Hansard Minutes", "uploads/hansard-1.pdf"),
+    ]
+
+    for tmp in content:
+        item = Content()
+        item.type = tmp[0]
+        item.title = tmp[1]
+        item.url = tmp[2]
+        db.session.add(item)
+
+    revisions = [
+        ('B6 2010', 'uploads/revision-1.pdf'),
+        ('B6B 2010', 'uploads/revision-2.pdf'),
+        ('B6C 2010', 'uploads/revision-3.pdf'),
+        ('B6D 2010', 'uploads/revision-4.pdf'),
+    ]
+
+    for tmp in revisions:
+        revision = Revision()
+        revision.name = tmp[0]
+        revision.url = tmp[1]
+        db.session.add(revision)
+
+    sessions = [
+        (datetime.date(2012, 5, 3), ),
+        (datetime.date(2012, 5, 6), ),
+        (datetime.date(2011, 6, 20), ),
+        (datetime.date(2013, 8, 20), ),
+    ]
+
+    for tmp in sessions:
+        item = Session()
+        item.date_end= tmp[0]
+        db.session.add(item)
+
+    resolutions = [
+        ("vote", "pass", 235, 85, 2),
+        ("vote", "pass", None, None, None),
+        ("signing", "fail", 235, 85, 2),
+    ]
+
+    for tmp in resolutions:
+        resolution = Resolution()
+        resolution.type = tmp[0]
+        resolution.outcome = tmp[1]
+        resolution.count_for = tmp[2]
+        resolution.count_against = tmp[3]
+        resolution.count_abstain = tmp[4]
+        db.session.add(resolution)
 
     db.session.commit()
     return
