cae877fe04804146673f4f3812397c3bad4b401a
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2013-11-20 15:45:29 +0200

    Make admin interface work again.

--- pmg_backend/admin.py
+++ pmg_backend/admin.py
@@ -9,15 +9,8 @@ from wtforms.fields import SelectField, TextAreaField
 upload_path = app.config['UPLOAD_PATH']
 
 
-#class MyModelView(ModelView):
-#    create_template = 'admin/model/my_create.html'
-#
-#    def __init__(self, model, session, **kwargs):
-#        super(MyModelView, self).__init__(model, session, **kwargs)
-
-
 class BillView(ModelView):
-    form_excluded_columns = ('events', )
+    form_excluded_columns = ('content', )
     form_overrides = dict(bill_type=SelectField, objective=TextAreaField)
     form_args = dict(
         # Pass the choices to the `SelectField`
@@ -31,19 +24,11 @@ class BillView(ModelView):
     )
 
 
-class EventView(ModelView):
+class EntryView(ModelView):
     form_overrides = dict(notes=TextAreaField)
     inline_models = [
         (
-            Version, {
-                'form_overrides' : {'url': form.FileUploadField},
-                'form_args' : {
-                    'url': {'label': 'File', 'base_path': upload_path}
-                }
-            }
-        ),
-        (
-            Content, {
+            Tag, {
                 'form_overrides' : {'url': form.FileUploadField},
                 'form_args' : {
                     'url': {'label': 'File', 'base_path': upload_path}
@@ -57,10 +42,11 @@ admin = Admin(app, name='PMG Bill Tracker', base_template='admin/my_master.html'
 admin.add_view(FileAdmin(upload_path, '/uploads/', name='Uploads'))
 
 admin.add_view(BillView(Bill, db.session, name="Bills"))
-admin.add_view(EventView(Event, db.session, name="Events"))
+admin.add_view(EntryView(Entry, db.session, name="Entries"))
 
 # views for CRUD admin
 admin.add_view(ModelView(Agent, db.session, name="Agents"))
 admin.add_view(ModelView(Location, db.session, name="Locations"))
 admin.add_view(ModelView(Stage, db.session, name="Stages"))
-admin.add_view(ModelView(ContentType, db.session, name='Content Types'))
+
+admin.add_view(ModelView(Tag, db.session, name="Tags"))
