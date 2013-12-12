c4721e77d34c7de520e693c5a53b494c7f2e6bd6
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2013-12-12 17:31:09 +0200

    Find relations between committee meeting reports and bills using Adi's regex. Save results to file.

--- pmg_scrapers/pmg/committee_reports.py
+++ pmg_scrapers/pmg/committee_reports.py
@@ -7,59 +7,39 @@ from BeautifulSoup import BeautifulSoup
 from dateutil import parser as dateparser
 from datetime import datetime
 from pmg_scrapers import scrapertools
+import simplejson
+import re
+
+re_bill = re.compile("""
+        (            # Capture the following match
+                     # Match a bill code that looks something like [B9B - 2004]
+        \[           # Match the first bracket
+        \s*          # White space after the bracket
+        B            # Bills start with a B
+        \s*          # More whitespace after the B
+        [0-9]+[A-Z]* # Bill number e.g. 10A (the letter might be missing)
+        \s*-\s*      # The hyphen might be surrounded by whitespaced
+        [0-9]{4}     # Search for a 4 digit year
+        \s*\]        # Round off by finding a closing square bracket preceeded by optional spaces
+        )            # close the capture
+""", re.IGNORECASE | re.VERBOSE)
+
 
 class ReportParser(object):
-    def __init__(self):
-        self.state_fn = self.start_state
+    def __init__(self, report_url):
+        self.report_url = report_url
         self.bills = []
 
-    def start_state(self, fragment):
-        if not fragment.find("strong"):
-            return True
-
-        self.state_fn = self.header_state
-        return False
-
-
-    def header_state(self, fragment):
-        if not fragment.find("strong"):
-            raise Exception("Unknown state")
-            
-        self.current_bill = {}
-        self.bills.append(self.current_bill)
-
-        text = fragment.text
-        text = text.replace(u"\u2013", "-")
-        parts = text.split("-")
-        self.current_bill["bill_number"] = parts[0].strip()
-        self.current_bill["bill_name"] = parts[1].strip()
-        if len(parts) == 3:
-            self.current_bill["introduced_by"] = parts[2].strip()
-
-        self.state_fn = self.version_state
-        return True
-
-    def version_state(self, fragment):
-        link = fragment.find("a")
-        if link: 
-            versions = self.current_bill.setdefault("versions", [])
-            url = link["href"]
-            versions.append({
-                "url" : link["href"],
-                "name" : link.text,
-                "date" : dateparser.parse(fragment.findAll("td")[1].text),
-            })
-            self.state_fn = self.version_state
-            return True
-        else:
-            self.state_fn = self.header_state
-            return False
+    def find_bills(self):
+        html = scrapertools.URLFetcher("http://www.pmg.org.za" + self.report_url).html
+        for res in re_bill.finditer(html):
+            bill_str = res.group(1).replace(" ", "")[1:-1]
+            if not bill_str in self.bills:
+                self.bills.append(bill_str)
+        return self.bills
 
 
 class ReportPager(object):
-    current_url = None
-    current_page = None
-
     def __init__(self, start_url):
         self.current_url = start_url
         self.current_page = scrapertools.URLFetcher("http://www.pmg.org.za" + self.current_url).html
@@ -114,15 +94,30 @@ class CommitteePager(object):
 
 if __name__ == "__main__":
 
-    # committee_pager = CommitteePager()
-    # for (index, (list_name, href, name)) in enumerate(committee_pager.next_committee):
-    #     if index == 0:
-    #         sample = list_name, href, name
-    #     print("\t" + str(index+1) + ": " + name)
-
-    list_name, href, name = (u'National Assembly Committees', u'/committees/Agriculture, Forestry and Fisheries', u'Agriculture, Forestry and Fisheries')
-
-    report_pager = ReportPager(href)
-    for report in report_pager.next_report:
-        print(report)
-
+    f = open('committee_meetings.json', 'w')
+    f.write('[\n')
+    count = 0
+    committee_pager = CommitteePager()
+    for (i, (list_name, href_committee, name)) in enumerate(committee_pager.next_committee):
+        print("\t" + str(i+1) + ": " + name)
+        report_pager = ReportPager(href_committee)
+        for (j, (date, title, href_report)) in enumerate(report_pager.next_report):
+            print("\t\t" + date + " - " + title)
+            report_parser = ReportParser(href_report)
+            bills = report_parser.find_bills()
+            if bills:
+                if count != 0:
+                    f.write(",\n")
+                count += 1
+                print("\t\t" + str(count) + " - " + str(bills))
+                entry = {
+                    "bills": bills,
+                    "href": href_report,
+                    "date": date,
+                    "title": title,
+                    "location": list_name,
+                    "committee": name,
+                    }
+                f.write(simplejson.dumps(entry, indent=4))
+    f.writeln(']')
+    f.close()
