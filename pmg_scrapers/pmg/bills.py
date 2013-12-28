3d425cc5147c14bbadabca9046f3cca545d0f0cc
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2013-12-28 16:53:44 +0200

    Improve identification and classification of bills.

--- pmg_scrapers/pmg/bills.py
+++ pmg_scrapers/pmg/bills.py
@@ -9,6 +9,7 @@ from BeautifulSoup import BeautifulSoup
 from dateutil import parser as date_parser
 from datetime import datetime
 import pmg_scrapers.scrapertools
+import string
 
 DEBUG = False
 
@@ -26,7 +27,8 @@ class BillParser(object):
     """
     def __init__(self):
         self.state_fn = self.start_state
-        self.bills = []
+        self.bills = {}
+        self.drafts = []
         self.current_bill = {}
 
     def start_state(self, fragment):
@@ -46,24 +48,25 @@ class BillParser(object):
         if not fragment.find("strong"):
             raise Exception("Unknown state")
 
-        self.bills.append(self.current_bill)
+        if self.current_bill:
+            try:
+                self.bills[self.current_bill["bill_id"]] = self.current_bill
+            except KeyError:
+                if self.current_bill['bill_type'] == "Draft":
+                    self.drafts.append(self.current_bill)
+                else:
+                    print("Bill cannot be identified")
+                    print(json.dumps(self.current_bill, indent=4, default=pmg_scrapers.scrapertools.handler))
+                    raise
+            if DEBUG:
+                print(json.dumps(self.current_bill, indent=4, default=pmg_scrapers.scrapertools.handler))
+            self.current_bill = {}
 
         text = fragment.text
-
-        text = text.replace(u"\u2013", "-")  # TODO: do this for all years
         parts = text.split("-")
-        self.current_bill["bill_number"] = parts[0].strip()
         self.current_bill["bill_name"] = parts[1].strip()
         if len(parts) == 3:
             self.current_bill["introduced_by"] = parts[2].strip()
-        if DEBUG:
-            print(text)
-            print("\tbill_number: " + self.current_bill["bill_number"])
-            print("\tbill_name: " + self.current_bill["bill_name"])
-            try:
-                print("\tintroduced_by: " + self.current_bill["introduced_by"])
-            except KeyError:
-                pass
 
         self.state_fn = self.version_state
         return True
@@ -76,16 +79,94 @@ class BillParser(object):
         if link: 
             versions = self.current_bill.setdefault("versions", [])
             url = link["href"]
-            versions.append({
+            if not self.current_bill.get("bill_type"):
+                tmp = link.text
+                info = parse_bill_code(tmp)
+                self.current_bill = dict(self.current_bill.items() + info.items())
+            version = {
                 "url" : link["href"],
                 "name" : link.text,
                 "date" : date_parser.parse(fragment.findAll("td")[1].text).date(),
-            })
+            }
+            versions.append(version)
             self.state_fn = self.version_state
             return True
         else:
             self.state_fn = self.header_state
             return False
+
+
+def parse_bill_code(code):
+    """
+    Extract information about a bill, from codes such as:
+        B6-2010
+        B6F-2010
+        B4-2010 - as enacted
+        B - 2010
+        PMB5-2013
+    """
+
+    code = code.strip()
+    out = {}
+
+    if not "-" in code:
+        if code[-4::].isdigit():
+            code = code[0:-4] + "-" + code[-4::]
+        else:
+            print("Error parsing code: " + code)
+            raise ValueError
+
+    components = code.split("-")
+
+    # extract year
+    year = None
+    for comp in components:
+        try:
+            year = int(comp)
+            break
+        except (IndexError, ValueError) as e:
+            pass
+    out["year"] = year
+
+    # extract bill number
+    bill_number = None
+    tmp = ""
+    tmp_str = components[0].strip()
+    for i in range(len(tmp_str)):
+        if tmp_str[i] in string.ascii_letters + " ":
+            pass
+        else:
+            tmp += tmp_str[i]
+    if len(tmp) > 0:
+        bill_number = int(tmp)
+    out["bill_number"] = bill_number
+
+    # extract bill type
+    bill_type = None
+    if code.startswith("PMB"):
+        bill_type = "PMB"
+    elif code.startswith("B"):
+        bill_type = "B"
+    if "as enacted" in code:
+        bill_type = "Act"
+    if not bill_number:
+        bill_type = "Draft"
+    out['bill_type'] = bill_type
+
+    # extract version
+    version = None
+    tmp = components[0]
+    if bill_number and bill_number != "X":
+        version = tmp.split(str(bill_number))[-1]
+
+    # assemble id string
+    if bill_type and bill_number and year:
+        prefix = "B"
+        if bill_type == "PMB":
+            prefix = "PMB"
+        bill_id = prefix + str(bill_number) + "-" + str(year)
+        out["bill_id"] = bill_id
+    return out
 
 
 class Pager(object):
@@ -106,6 +187,10 @@ class Pager(object):
 if __name__ == "__main__":
 
     DEBUG = True
+
+    # for code in ["B6-2010", "B6F-2010", "B4-2010 - as enacted", "B - 2010", "PMB5-2013"]:
+    #     print(code)
+    #     print(parse_bill_code(code))
 
     pager = Pager()
     bills = []
@@ -128,6 +213,5 @@ if __name__ == "__main__":
         # save extracted content for this page
         bills.extend(parser.bills)
 
-    if not DEBUG:
-        # do something with scraped data
-        print(json.dumps(bills, indent=4, default=pmg_scrapers.scrapertools.handler))
+    # # do something with scraped data
+    # print(json.dumps(bills, indent=4, default=pmg_scrapers.scrapertools.handler))
