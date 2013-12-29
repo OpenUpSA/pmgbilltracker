811466c3d24cee5c4989048bd32134247384128c
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2013-12-29 18:28:33 +0200

    Only add bill versions if they don't exist yet.

--- pmg_scrapers/main.py
+++ pmg_scrapers/main.py
@@ -4,8 +4,8 @@ from pmg_backend import db
 import simplejson
 
 
-def add_entry(data, bill_codes):
-    entry = Entry()
+def populate_entry(entry, data, bill_codes):
+    # populate bill relations
     for code in bill_codes:
         tmp_bill = Bill.query.filter(Bill.code==code).first()
         if tmp_bill:
@@ -13,9 +13,11 @@ def add_entry(data, bill_codes):
         else:
             print("Could not find related bill.")
             raise Exception
+    # populate required fields
     entry.type = data['entry_type']
     entry.date = data['date']
     entry.title = data['title']
+    # populate optional fields
     if data.get("description"):
         entry.description = data['description']
     if data.get("location"):
@@ -23,9 +25,9 @@ def add_entry(data, bill_codes):
     if data.get("stage"):
         entry.stage = data['stage']
     if data.get("url"):
-        entry.stage = data['url']
+        entry.url = data['url']
+    return entry
 
-    return
 
 def scrape_bills(DEBUG):
     from pmg import bills
@@ -35,34 +37,39 @@ def scrape_bills(DEBUG):
     print str(len(draft_list)) + " draft bills scraped"
 
     # save scraped bills to database
-    for bill_code, bill in bill_dict.iteritems():
-        tmp = Bill.query.filter(Bill.code==bill_code).first()
-        if tmp is None:
-            tmp = Bill()
-            tmp.code = bill_code
-        tmp.name = bill['bill_name']
-        if bill.get('introduced_by'):
-            tmp.introduced_by = bill['introduced_by']
-        tmp.year = bill['year']
-        tmp.bill_type = bill['bill_type']
-        db.session.add(tmp)
+    for bill_code, bill_data in bill_dict.iteritems():
+        bill = Bill.query.filter(Bill.code==bill_code).first()
+        if bill is None:
+            bill = Bill()
+            bill.code = bill_code
+        bill.name = bill_data['bill_name']
+        if bill_data.get('introduced_by'):
+            bill.introduced_by = bill_data['introduced_by']
+        bill.year = bill_data['year']
+        bill.bill_type = bill_data['bill_type']
+        db.session.add(bill)
         db.session.commit()
         # save related bill versions
-        for data in bill['versions']:
-            data["entry_type"] = "bill_version"
-            add_entry(data, [bill_code, ])
+        for entry_data in bill_data['versions']:
+            entry = Entry.query.filter(Entry.url==entry_data['url']).first()  # Look for pre-existing entry.
+            if entry is None:
+                entry = Entry()  # Create new entry.
+            entry_data["entry_type"] = "bill_version"
+            entry = populate_entry(entry, entry_data, [bill_code, ])
+            db.session.add(entry)
+            db.session.commit()
 
     # save scraped draft bills to database
     for draft in draft_list:
-        tmp = Bill.query.filter(Bill.name==draft['bill_name']).filter(Bill.year==draft['year']).first()
-        if tmp is None:
-            tmp = Bill()
-            tmp.name = draft['bill_name']
-            tmp.year = draft['year']
-        tmp.bill_type = draft['bill_type']
-        if bill.get('introduced_by'):
-            tmp.introduced_by = draft['introduced_by']
-        db.session.add(tmp)
+        bill = Bill.query.filter(Bill.name==draft['bill_name']).filter(Bill.year==draft['year']).first()
+        if bill is None:
+            bill = Bill()
+            bill.name = draft['bill_name']
+            bill.year = draft['year']
+        bill.bill_type = draft['bill_type']
+        if draft.get('introduced_by'):
+            bill.introduced_by = draft['introduced_by']
+        db.session.add(bill)
         db.session.commit()
 
     return
@@ -82,8 +89,8 @@ if __name__ == "__main__":
 
     DEBUG = True
 
-    db.drop_all()
-    db.create_all()
+    # db.drop_all()
+    # db.create_all()
 
     scrape_bills(DEBUG)
 
