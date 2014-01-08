63a364ef47575dd4ee8a86377c8057b4588a4d23
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-01-08 13:33:08 +0200

    Start from most recent year that has entries when showing a bill index page.

--- pmg_frontend/views.py
+++ pmg_frontend/views.py
@@ -17,50 +17,54 @@ def root():
 @app.route('/bills/<year>/')
 @app.route('/bills/<bill_type>/all/')
 @app.route('/bills/<bill_type>/<year>/')
-def index(year=date.today().year, bill_type=None):
+def index(year=None, bill_type=None):
     """
     Display a list of available bills, with some summary info and a link to each bill's detail page.
     """
 
     try:
-        year = int(year)
+        if year is not None:
+            year = int(year)
     except ValueError as e:
         logger.debug(e)
         abort(400)
 
-    current_year = date.today().year
+    start_year = date.today().year
 
     tmp = "bill"
     page_title = "All bills"
-    year_list = range(current_year, 2005, -1)
-    if bill_type and bill_type.lower() in ["pmb", "draft", "current"]:
-        tmp = bill_type.lower()
-        if bill_type.lower() == "pmb":
-            page_title = "Private Member Bills"
-            year_list = range(current_year, 2011, -1)
-        elif bill_type.lower() == "draft":
-            page_title = "Draft Bills"
-            year_list = range(current_year, 2007, -1)
-        elif bill_type.lower() == "current":
-            page_title = "Current Bills"
-            year = None
-            year_list = []
+    bills = []
+    while (not bills) and (start_year > 2007):
+        year_list = range(start_year, 2005, -1)
+        if bill_type and bill_type.lower() in ["pmb", "draft", "current"]:
+            tmp = bill_type.lower()
+            if bill_type.lower() == "pmb":
+                page_title = "Private Member Bills"
+                year_list = range(start_year, 2011, -1)
+            elif bill_type.lower() == "draft":
+                page_title = "Draft Bills"
+                year_list = range(start_year, 2007, -1)
+            elif bill_type.lower() == "current":
+                page_title = "Current Bills"
+                start_year = None
+                year_list = []
+        logger.debug("landing page called")
+        status_dict = {
+            "na": ("in progress", "label-primary"),
+            "ncop": ("in progress", "label-primary"),
+            "assent": ("sent to the president", "label-warning"),
+            "enacted": ("signed into law", "label-success"),
+            "withdrawn": ("withdrawn", "label-default"),
+            }
+        if start_year:
+            r = requests.get("http://" + API_HOST + "/" + tmp + "/year/" + str(start_year) + "/")
+        else:
+            r = requests.get("http://" + API_HOST + "/" + tmp + "/")
+        bills = r.json()
+        if not bills:
+            start_year -= 1
 
-    logger.debug("landing page called")
-    status_dict = {
-        "na": ("in progress", "label-primary"),
-        "ncop": ("in progress", "label-primary"),
-        "assent": ("sent to the president", "label-warning"),
-        "enacted": ("signed into law", "label-success"),
-        "withdrawn": ("withdrawn", "label-default"),
-        }
-    if year:
-        r = requests.get("http://" + API_HOST + "/" + tmp + "/year/" + str(year) + "/")
-    else:
-        r = requests.get("http://" + API_HOST + "/" + tmp + "/")
-    bills = r.json()
-
-    return render_template('index.html', title=page_title, bill_type=bill_type, year_list=year_list, year=year, bills=bills, status_dict=status_dict, api_host=API_HOST)
+    return render_template('index.html', title=page_title, bill_type=bill_type, year_list=year_list, year=start_year, bills=bills, status_dict=status_dict, api_host=API_HOST)
 
 
 @app.route('/bills/')
