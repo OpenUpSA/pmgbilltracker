e6cb86d035d97f62055d1cf6d409e9e58daf5f76
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-01-13 14:14:59 +0200

    Include space for scraper feedback on admin landing page.

--- pmg_backend/admin.py
+++ pmg_backend/admin.py
@@ -1,9 +1,10 @@
 from pmg_backend import app
-from flask.ext.admin import Admin, form, BaseView, expose
+from flask.ext.admin import Admin, form, BaseView, AdminIndexView, expose
 from flask.ext.admin.contrib.sqla import ModelView
 from models import *
 from flask.ext.admin.contrib.fileadmin import FileAdmin
 from wtforms.fields import SelectField, TextAreaField
+import datetime
 
 
 class BillView(ModelView):
@@ -19,7 +20,7 @@ class BillView(ModelView):
                 ("Section 75 (Ordinary Bills not affecting the provinces)", "Section 75 (Ordinary Bills not affecting the provinces)"),
                 ("Section 76 (Ordinary Bills affecting the provinces)", "Section 76 (Ordinary Bills affecting the provinces)"),
                 ("Other", "Other"),
-            ]
+                ]
         ),
         status=dict(
             choices=[
@@ -34,19 +35,19 @@ class BillView(ModelView):
     )
 
 entry_types = [
-        "gazette",
-        "memorandum",
-        "greenpaper",
-        "whitepaper",
-        "draft",
-        "bill",
-        "pmg-meeting-report",
-        "public-hearing-report",
-        "committee-report",
-        "hansard-minutes",
-        "vote-count",
-        "other",
-        ]
+    "gazette",
+    "memorandum",
+    "greenpaper",
+    "whitepaper",
+    "draft",
+    "bill",
+    "pmg-meeting-report",
+    "public-hearing-report",
+    "committee-report",
+    "hansard-minutes",
+    "vote-count",
+    "other",
+    ]
 
 entry_type_choices = []
 for entry_type in entry_types:
@@ -66,12 +67,60 @@ class EntryView(ModelView):
                 (1, "National Assembly (NA)"),
                 (2, "National Council of Provinces (NCOP)"),
                 (3, "President's Office"),
-            ]
+                ]
         )
     )
     # TODO: paste raw url
 
-admin = Admin(app, name='PMG Bill Tracker', base_template='admin/my_master.html')
+
+class HomeView(AdminIndexView):
+    @expose("/")
+    def index(self):
+        stats = {
+            "date": datetime.date.today(),
+            "start_time": datetime.time(hour=1, minute=0),
+            "duration": datetime.timedelta(hours=1, minutes=23),
+            "bills": {
+                "total": 111,
+                "new": 1,
+                "errors": [
+                    "one",
+                    "two",
+                    "three",
+                    ]
+            },
+            "committees": {
+                "total": 111,
+                "new": 1,
+                "errors": [
+                    "one",
+                    "two",
+                    "three",
+                    ]
+            },
+            "committee_meeting_reports": {
+                "total": 111,
+                "new": 1,
+                "errors": [
+                    "one",
+                    "two",
+                    "three",
+                    ]
+            },
+            "hansards": {
+                "total": 111,
+                "new": 1,
+                "errors": [
+                    "one",
+                    "two",
+                    "three",
+                    ]
+            },
+            }
+        return self.render('admin/home.html', stats=stats)
+
+
+admin = Admin(app, name='PMG Bill Tracker', base_template='admin/my_master.html', index_view=HomeView(name='Home'))
 
 admin.add_view(BillView(Bill, db.session, name="Bills"))
 admin.add_view(EntryView(Entry, db.session, name="Entries"))
