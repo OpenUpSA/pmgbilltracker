43a613e29190e4a50fdc12c34e4d85338b58272b
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-04-07 11:09:02 +0200

    In Progress: Bill status event scraping.

--- pmg_scrapers/bill_status.py
+++ pmg_scrapers/bill_status.py
@@ -1,155 +1,64 @@
+"""
+Scrapes committees from PMG e.g.
+http://www.pmg.org.za/committees
+"""
 from __future__ import print_function
-import datetime
+from BeautifulSoup import BeautifulSoup, Tag
+
+import scrapertools
+from pmg_scrapers import logger
 from pmg_backend.models import *
 from pmg_backend import db
-from random import shuffle
-from scrapertools import analyze_bill_code
-from dateutil import parser as date_parser
-import csv
-from pmg_scrapers import logger
-
-def find_current_bills():
-    """
-    Update status of most recent set of bills from http://pmg.org.za/billsstatus/proceedings, via
-    the csv at /data/current_status.csv
-    """
-
-    data = []
-    with open("../data/current_status.csv", 'Ur') as f:
-        reader = csv.reader(f)
-        headers = reader.next()
-
-        for i, row in enumerate(reader):
-
-            entry = row
-
-            # fix bill types
-            if entry[0].startswith("PM"):
-                entry[0] = "PMB" + entry[0][2::]
-            elif not entry[0].startswith("B"):
-                entry[0] = "B" + entry[0]
-            tmp_code = entry[0]
-            tmp_status = entry[1].lower()
-
-            # clean bill code
-            tmp = analyze_bill_code(tmp_code)
-            code = tmp["code"]
-
-            logger.info(code + " " + str(entry))
-
-            bill = Bill.query.filter(Bill.code==code).first()
-            available_status = {
-                "act": "enacted",
-                "": None,
-                "pc": "na",
-                "sc": "ncop",
-                "intro": "na",
-            }
-
-            if available_status.get(tmp_status):
-                tmp_status = available_status[tmp_status]
-            bill.status = tmp_status
-            db.session.add(bill)
-    db.session.commit()
-    return
+import json
 
 
-def find_enacted_bills():
-    """
-    Set status of bills that have already been enacted.
-    """
+class StatusScraper(object):
 
-    for bill in Bill.query.all():
-        for entry in bill.entries:
-            if entry.type == "act":
-                bill.status = "enacted"
-                db.session.add(bill)
-                logger.info("enacted: " + bill.name)
-                break
-    db.session.commit()
-    return
+    def __init__(self):
+        self.bill_status = {}
+        self.stats = {
+            "total_committees": 0,
+            "new_committees": 0,
+            "errors": []
+        }
+
+    def run_scraper(self):
+        """
+        Iterate through the bill status pages in /data
+        """
+
+        html = open('/Users/petrus/Desktop/code4sa/pmgbilltracker/data/bill_status_2007.html', 'r').read()
+        soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
+        # container = soup.find(id="committees-all")
+        status_tables = soup.findAll("table", {"class": "MsoNormalTable"})
+        for status_table in status_tables:
+            rows = status_table.findAll("tr")
+            # loop through table rows, skipping the header
+            for row in rows[1::]:
+                cells = row.findAll('td')
+                tmp_row = []
+                for i in range(len(cells)):
+                    cell = cells[i]
+                    tmp_cell = ""
+                    content_list = cell.findAll("p")
+                    for item in content_list:
+                        for tag in item.recursiveChildGenerator():
+                            if isinstance(tag, Tag):
+                                content = tag.string
+                            else:
+                                content = tag
+                            if content:
+                                tmp_cell += content.encode('ascii', 'replace') + "\n"
+                    tmp_cell = tmp_cell[0:-1]
+                    tmp_row.append(tmp_cell)
+                print(tmp_row)
+        return
 
 
-def handle_assent():
-    """
-    Add entries relating to a bill's assent from http://pmg.org.za/billsstatus/proceedings, via
-    the csv at /data/bill_assent_dates.csv
-    """
 
-    with open("../data/bill_assent_dates.csv", 'Ur') as f:
-        data = list(list(rec) for rec in csv.reader(f, delimiter=','))
-
-    president = Agent.query.filter(Agent.name == "The President").first()
-
-    for i in range(len(data)):
-
-        # ignore column title row
-        if i==0:
-            continue
-
-        entry = data[i]
-
-        # fix bill types
-        if entry[0].startswith("PM"):
-            entry[0] = "PMB" + entry[0][2::]
-        elif not entry[0].startswith("B"):
-            entry[0] = "B" + entry[0]
-        tmp_code = entry[0]
-
-        # clean bill code
-        tmp = analyze_bill_code(tmp_code)
-        if tmp:
-            code = tmp["code"]
-        else:
-            logger.error("Error analyzing bill code " + tmp_code)
-            continue
-
-        logger.info(code + " " + str(entry))
-
-        bill = Bill.query.filter(Bill.code==code).first()
-        if bill is None:
-            logger.error("Error finding bill " + code)
-            continue
-
-        try:
-            act_no = unicode(entry[1])
-            assent_date = unicode(entry[2])
-            # convert date to python date object
-            try:
-                assent_date = date_parser.parse(assent_date).date()
-            except Exception:
-                logger.error("Error parsing date " + entry[2])
-                continue
-            if entry[3] and len(entry[3]) > 2:
-                gazette = unicode(entry[3])
-        except UnicodeDecodeError:
-            logger.error("Unicode error: " + str(entry))
-            continue
-
-        # update bill record
-        bill.status = "enacted"
-        if gazette:
-            bill.gazette = gazette
-        db.session.add(bill)
-
-        # add relevant entry in bill history
-        tmp_entry = Entry.query.join(Entry.bills).filter(Bill.code==code).filter(Entry.type=="assent").first()
-        if not tmp_entry:
-            tmp_entry = Entry()
-            tmp_entry.bills.append(bill)
-        tmp_entry.date = assent_date
-        tmp_entry.type = "assent"
-        tmp_entry.location = 4
-        tmp_entry.title = "Signed into law by the President."
-        tmp_entry.agent = president
-        if act_no and gazette:
-            tmp_entry.description = "Enacted as Act " + act_no + ". Refer to Government Gazette " + gazette + "."
-        db.session.add(tmp_entry)
-    db.session.commit()
-    return
 
 if __name__ == "__main__":
 
-    find_current_bills()
-    find_enacted_bills()
-    handle_assent()
+    status_scraper = StatusScraper()
+    status_scraper.run_scraper()
+    logger.info(json.dumps(status_scraper.stats, indent=4))
